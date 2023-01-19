0f73697f65862e1a6c4fcbce3b4b8c899ed7913d
Naif <knifffe@yandex.ru>
2023-01-19 12:28:45 +0300

    +3

--- main.ts
+++ main.ts
@@ -1,19 +1,22 @@
+let H = 0
+let S = 0
+let L = 0
 let RGBColor = 0
 let blue = 0
 let green = 0
 let red = 0
 basic.forever(function () {
-    for (let index = 0; index <= 360; index++) {
-        RGBColor = neopixel.hsl(index, 100, 50)
-        blue = RGBColor % 255
-        green = RGBColor / 65535 % 255
-        red = RGBColor / 65535
-        red = Math.map(red, 0, 255, 1023, 0)
-        green = Math.map(green, 0, 255, 1023, 0)
-        blue = Math.map(blue, 0, 255, 1023, 0)
-        pins.analogWritePin(AnalogPin.P0, blue)
-        pins.analogWritePin(AnalogPin.P1, green)
-        pins.analogWritePin(AnalogPin.P2, red)
-        basic.pause(10)
-    }
+    H = Math.map(pins.analogReadPin(AnalogPin.P3), 0, 1023, 0, 360)
+    S = Math.map(pins.analogReadPin(AnalogPin.P4), 0, 1023, 0, 100)
+    L = Math.map(pins.analogReadPin(AnalogPin.P10), 0, 1023, 0, 100)
+    RGBColor = neopixel.hsl(H, S, L)
+    blue = RGBColor % 255
+    green = RGBColor / 65535 % 255
+    red = RGBColor / 65535
+    red = Math.map(red, 0, 255, 1023, 0)
+    green = Math.map(green, 0, 255, 1023, 0)
+    blue = Math.map(blue, 0, 255, 1023, 0)
+    pins.analogWritePin(AnalogPin.P0, blue)
+    pins.analogWritePin(AnalogPin.P1, green)
+    pins.analogWritePin(AnalogPin.P2, red)
 })

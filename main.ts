9599a9e8969cf174f84010432c8c68479191c184
Naif <knifffe@yandex.ru>
2023-01-20 20:47:39 +0300

    fix

--- main.ts
+++ main.ts
@@ -1,17 +1,18 @@
-let H = 0
-let S = 0
-let L = 0
-let RGBColor = 0
-let blue = 0
-let green = 0
 let red = 0
+let green = 0
+let blue = 0
+let RGBColor = 0
+let L = 0
+let S = 0
+let H = 0
+led.enable(false)
 basic.forever(function () {
     H = Math.map(pins.analogReadPin(AnalogPin.P3), 0, 1023, 0, 360)
     S = Math.map(pins.analogReadPin(AnalogPin.P4), 0, 1023, 0, 100)
     L = Math.map(pins.analogReadPin(AnalogPin.P10), 0, 1023, 0, 100)
     RGBColor = neopixel.hsl(H, S, L)
     blue = RGBColor % 255
-    green = RGBColor / 65535 % 255
+    green = RGBColor % 65535 / 255
     red = RGBColor / 65535
     red = Math.map(red, 0, 255, 1023, 0)
     green = Math.map(green, 0, 255, 1023, 0)
